353e80ac336469dcd305103a36fa67380d995354
Pablov <juan.vasquez@valposystem.com>
2024-04-25 12:50:38 -0400

    Modificaciones.
    1) Agregar viewBinding en archivo build.gradle.kts.
    
    2) Agregar librería androidx.navigation en archivo build.gradle.kts.
    
    3) Creación de paquete navigation.
    
    4) Creación de ménu de navegación.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,10 @@ android {
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
+    //Agregar
+    buildFeatures{
+        viewBinding=true;
+    }
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -37,6 +40,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
